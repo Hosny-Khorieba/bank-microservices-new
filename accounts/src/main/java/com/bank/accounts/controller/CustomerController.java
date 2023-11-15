a9cbb6d331a0940e705d9991430ddf0f1a2bc6cb
hosny.emad <hemad@ejada.com>
2023-11-15 15:59:03 +0200

    after setting all up observability and monitoring

--- accounts/src/main/java/com/bank/accounts/controller/CustomerController.java
+++ accounts/src/main/java/com/bank/accounts/controller/CustomerController.java
@@ -59,8 +59,9 @@ public class CustomerController {
                                                                    @RequestParam
                                                                    @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
                                                                            String mobileNumber){
-        logger.debug("Bank-correlation-id found: {} ", correlationId);
+        logger.debug("fetchCustomerDetails start");
         CustomerDetailsDto customerDetailsDto = iCustomersService.fetchCustomerDetails(mobileNumber, correlationId);
+        logger.debug("fetchCustomerDetails end");
         return ResponseEntity.status(HttpStatus.SC_OK).body(customerDetailsDto);
 
     }
